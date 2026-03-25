84ada9969d9e8d4fda83c924806adc4076a1cf49
dragonnnii <159734819+dragonnnii@users.noreply.github.com>
2026-03-25 19:41:46 +0300

    Add files via upload

---
+++ Core/Framework/SpellProjectile.cs
@@ -0,0 +1,266 @@
+using System;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Netcode;
+using StardewValley;
+using StardewValley.Monsters;
+using StardewValley.Projectiles;
+using StardewValley.TerrainFeatures;
+
+namespace WizardrySkill.Core.Framework.Game
+{
+
+    // Copied and modified from vanilla's debuff projectile to make our own projectiles!
+    public class SpellProjectile : Projectile
+    {
+        //
+        // Summary:
+        //     The buff ID to apply to players hit by this projectile.
+        public readonly NetString Debuff = new NetString();
+
+        public NetBool WavyMotion = new NetBool(value: true);
+
+        public NetInt DebuffIntensity = new NetInt(-1);
+
+
+        //
+        // Summary:
+        //     The amount of damage caused when this projectile hits a monster or player.
+        public readonly NetInt DamageToFarmer = new NetInt();
+
+        private float PeriodicEffectTimer;
+        private bool Explosion;
+
+        //
+        // Summary:
+        //     Construct an empty instance.
+        public SpellProjectile()
+        {
+        }
+
+        //
+        // Summary:
+        //     Construct an instance.
+        //
+        // Parameters:
+        //   debuff:
+        //     The debuff ID to apply to players hit by this projectile.
+        //
+        //   spriteIndex:
+        //     The index of the sprite to draw in StardewValley.Projectiles.Projectile.projectileSheetName.
+        //
+        //
+        //   bouncesTillDestruct:
+        //     The number of times the projectile can bounce off walls before being destroyed.
+        //
+        //
+        //   tailLength:
+        //     The length of the tail which trails behind the main projectile.
+        //
+        //   rotationVelocity:
+        //     The rotation velocity.
+        //
+        //   xVelocity:
+        //     The speed at which the projectile moves along the X axis.
+        //
+        //   yVelocity:
+        //     The speed at which the projectile moves along the Y axis.
+        //
+        //   startingPosition:
+        //     The pixel world position at which the projectile will start moving.
+        //
+        //   location:
+        //     The location containing the projectile.
+        //
+        //   owner:
+        //     The character who fired the projectile.
+        public SpellProjectile(int damage, string debuff, int spriteIndex, int bouncesTillDestruct, int tailLength, float rotationVelocity, float xVelocity, float yVelocity, Vector2 startingPosition, GameLocation location = null, Character owner = null, bool hitsMonsters = false, bool playDefaultSoundOnFire = true, string sound = "", bool explosion = false)
+            : this()
+        {
+            this.theOneWhoFiredMe.Set(location, owner);
+            this.Debuff.Value = debuff;
+            this.currentTileSheetIndex.Value = spriteIndex;
+            this.bouncesLeft.Value = bouncesTillDestruct;
+            base.tailLength.Value = tailLength;
+            base.rotationVelocity.Value = rotationVelocity;
+            base.xVelocity.Value = xVelocity;
+            base.yVelocity.Value = yVelocity;
+            this.position.Value = startingPosition;
+            this.damagesMonsters.Value = hitsMonsters;
+            this.DamageToFarmer.Value = damage;
+            this.Explosion = explosion;
+
+            if (playDefaultSoundOnFire)
+            {
+                string sfx = string.IsNullOrEmpty(sound) ? "debuffSpell" : sound;
+
+                if (location == null)
+                    Game1.playSound(sfx);
+                else
+                    location.playSound(sfx);
+            }
+
+
+
+
+        }
+
+        protected override void InitNetFields()
+        {
+            base.InitNetFields();
+            this.NetFields.AddField(Debuff, "debuff").AddField(WavyMotion, "wavyMotion").AddField(DebuffIntensity, "debuffIntensity").AddField(DamageToFarmer, "damageToFarmer");
+        }
+
+        public override void updatePosition(GameTime time)
+        {
+            this.xVelocity.Value += this.acceleration.X;
+            this.yVelocity.Value += this.acceleration.Y;
+            this.position.X += this.xVelocity.Value;
+            this.position.Y += this.yVelocity.Value;
+            if (this.WavyMotion.Value)
+            {
+                this.position.X += (float)Math.Sin(time.TotalGameTime.Milliseconds * Math.PI / 128.0) * 8f;
+                this.position.Y += (float)Math.Cos(time.TotalGameTime.Milliseconds * Math.PI / 128.0) * 8f;
+            }
+        }
+
+        public override bool update(GameTime time, GameLocation location)
+        {
+
+            if (this.Debuff.Value == "frozen")
+            {
+                this.PeriodicEffectTimer += (float)time.ElapsedGameTime.TotalMilliseconds;
+                if (this.PeriodicEffectTimer > 50f)
+                {
+                    this.PeriodicEffectTimer = 0f;
+                    Game1.Multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite("TileSheets\\Projectiles", new Rectangle(32, 32, 16, 16), 9999f, 1, 1, position.Value, flicker: false, flipped: false, 1f, 0.01f, Color.White, 4f, 0f, 0f, 0f)
+                    {
+                        motion = Utility.getRandom360degreeVector(1f) + new Vector2(this.xVelocity.Value, yVelocity.Value),
+                        drawAboveAlwaysFront = true
+                    });
+                }
+            }
+
+            return base.update(time, location);
+        }
+
+        public override void behaviorOnCollisionWithPlayer(GameLocation location, Farmer player)
+        {
+            if (damagesMonsters.Value)
+            {
+                return;
+            }
+
+            if (this.Debuff.Value != null && player.CanBeDamaged() && Game1.random.Next(11) >= player.Immunity && !player.hasBuff("28") && !player.hasTrinketWithID("BasiliskPaw"))
+            {
+                if (Game1.player == player)
+                {
+                    player.applyBuff(this.Debuff.Value);
+                }
+            }
+
+            if (player.CanBeDamaged())
+            {
+                piercesLeft.Value--;
+            }
+
+            player.takeDamage(this.DamageToFarmer.Value, overrideParry: false, null);
+            this.ExplosionAnimation(location);
+        }
+
+        public override void behaviorOnCollisionWithTerrainFeature(TerrainFeature t, Vector2 tileLocation, GameLocation location)
+        {
+            this.ExplosionAnimation(location);
+
+            this.piercesLeft.Value--;
+
+        }
+
+        public override void behaviorOnCollisionWithOther(GameLocation location)
+        {
+            this.ExplosionAnimation(location);
+            this.piercesLeft.Value--;
+
+        }
+
+        protected virtual void ExplosionAnimation(GameLocation location)
+        {
+            if (!(this.Debuff.Value == "frozen"))
+            {
+                Game1.Multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite(352, Game1.random.Next(100, 150), 2, 1, position.Value, flicker: false, flipped: false));
+            }
+        }
+
+        public override void behaviorOnCollisionWithMonster(NPC n, GameLocation location)
+        {
+
+            if (!this.damagesMonsters.Value)
+            {
+                return;
+            }
+
+            Farmer playerWhoFiredMe = GetPlayerWhoFiredMe(location);
+            this.ExplosionAnimation(location);
+
+            if (n is Monster)
+            {
+                location.damageMonster(n.GetBoundingBox(), this.DamageToFarmer.Value, this.DamageToFarmer.Value + 1, isBomb: false, playerWhoFiredMe, isProjectile: true);
+                Utilities.AddEXP(playerWhoFiredMe, this.DamageToFarmer.Value >> 2);
+
+                if (this.currentTileSheetIndex.Value == 15)
+                {
+                    Utility.addRainbowStarExplosion(location, position.Value, 11);
+                }
+
+                if (!(n as Monster).IsInvisible)
+                {
+                    this.piercesLeft.Value--;
+                    if (this.Explosion)
+                    {
+                        location.explode(new Vector2(n.Tile.X, n.Tile.Y), 3, this.GetPlayerWhoFiredMe(location), damage_amount: this.DamageToFarmer.Value);
+                    }
+                }
+
+                if (this.Debuff.Value == "frozen" && (!(n is Leaper leaper) || !leaper.leaping.Value))
+                {
+                    if ((n as Monster).stunTime.Value < 51)
+                    {
+                        this.piercesLeft.Value--;
+                    }
+
+                    if ((n as Monster).stunTime.Value < this.DebuffIntensity.Value - 1000)
+                    {
+                        location.playSound("frozen");
+                        Game1.Multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite("LooseSprites\\Cursors2", new Rectangle(118, 227, 16, 13), new Vector2(0f, 0f), flipped: false, 0f, Color.White)
+                        {
+                            layerDepth = (n.StandingPixel.Y + 2) / 10000f,
+                            animationLength = 1,
+                            interval = this.DebuffIntensity.Value,
+                            scale = 4f,
+                            id = (int)(n.position.X * 777f + n.position.Y * 77777f),
+                            positionFollowsAttachedCharacter = true,
+                            attachedCharacter = n
+                        });
+                    }
+
+                (n as Monster).stunTime.Value = this.DebuffIntensity.Value;
+                }
+            }
+
+
+            
+        }
+
+        //
+        // Summary:
+        //     Get the player who fired this projectile.
+        //
+        // Parameters:
+        //   location:
+        //     The location containing the player.
+        public virtual Farmer GetPlayerWhoFiredMe(GameLocation location)
+        {
+            return this.theOneWhoFiredMe.Get(location) as Farmer ?? Game1.player;
+        }
+    }
+}
